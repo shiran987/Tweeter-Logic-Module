7f36df1f4b0b1bf6a5f9c00241a94986a5009cf9
shiran987 <shiransh987@gmail.com>
2020-11-08 11:09:11 +0200

    tweeter project

--- logic.js
+++ logic.js
@@ -1,17 +1,3 @@
-// const postIdCounter = function(posts){
-//     let postsCounter = 0
-//     for (let post in posts)
-//     postsCounter++
-//     return postsCounter
-// }
-
-// let commentIdCounter = function(posts){
-//     let commentCounter = 0
-//     for (let comment in posts.comments)
-
-// }
-
-
 const Tweeter = function () {
     let _posts = [
         {
@@ -91,27 +77,3 @@ const Tweeter = function () {
 
 const tweeter = Tweeter()
 
-tweeter.addPost("This is my own post!")
-console.log(tweeter.getPosts())
-//This should be added to the posts array:
-//{text: "This is my own post!", id: "p3", comments: []}
-
-tweeter.removePost("p1")
-console.log(tweeter.getPosts())
-//There should only be two posts in the post's array:
-//{text: "Aw man, I wanted to be first", id: "p2", comments: Array(3)}
-//{text: "This is my own post!", id: "p3", comments: []}
-
-tweeter.addComment("Damn straight it is!", "p3")
-tweeter.addComment("Second the best!", "p2")
-console.log(tweeter.getPosts())
-//This should be added to the third post's comments array:
-//{id: "c7", text: "Damn straight it is!"}
-
-//This should be added to the second post's comments array:
-//{id: "c8", text: "Second the best!"}
-
-tweeter.removeComment("p2", "c6")
-console.log(tweeter.getPosts())
-//This comment should be removed:
-//{id: "c6", text: "Haha second place what a joke."}
